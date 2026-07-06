c368f4e673f19e6db2991d298402be2c3d59fdce
ahmad khurshed <ahmadkhurshed311@gmail.com>
2026-07-06 12:30:07 +0500

    add .bat file

---
+++ start-project.bat
@@ -0,0 +1,15 @@
+@echo off
+
+
+:: ===========================
+:: Setup React.js
+:: ===========================
+
+echo Checking React dependencies...
+npm install
+
+:: ===========================
+:: Start Application
+:: ===========================
+
+npm run dev
